1e932fd70141ad4aef2298ccfbd6a9a7c040033c
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2017-09-18 12:31:52 +0700

    change character limit

--- database/migrations/20170916_131753_users.go
+++ database/migrations/20170916_131753_users.go
@@ -20,7 +20,7 @@ func init() {
 // Run the migrations
 func (m *Users_20170916_131753) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	m.SQL("CREATE TABLE users(id SERIAL PRIMARY KEY, name TEXT NOT NULL, profession TEXT NOT NULL, dream TEXT NOT NULL, hobby TEXT NOT NULL, website_url TEXT NOT NULL, phone TEXT DEFAULT NULL, avatar_url TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, token TEXT NOT NULL, created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at timestamp NOT NULL)")
+	m.SQL("CREATE TABLE users(id SERIAL PRIMARY KEY, name name character varying(25), profession name character varying(25), dream name character varying(25), hobby name character varying(25), website_url TEXT NOT NULL, phone name character varying(15), avatar_url TEXT NOT NULL, email name character varying(35), password TEXT NOT NULL, token TEXT NOT NULL, created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at timestamp NOT NULL)")
 }
 
 // Reverse the migrations
